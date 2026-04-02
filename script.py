2b9bd4d267a39786576bb4c9cec4d40f5ce7aaa9
cristianleonmeza <cris.leon.meza@gmail.com>
2026-04-02 10:50:32 -0500

    numpy example added

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
 print("Hello from Python")
+
+import numpy as np
+import pandas as pd
+
+# Create a NumPy array
+arr = np.array([1, 2, 3, 4])
+
+# Create a DataFrame
+df = pd.DataFrame({
+    "numbers": arr,
+    "squared": arr**2
+})
+
+print(df)
